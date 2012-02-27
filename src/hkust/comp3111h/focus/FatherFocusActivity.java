144fb1e798c1495b616aa155b05058b1b6517559
Yiufung Cheong <yzhangak@ust.hk>
2012-02-28 00:31:25 +0800

    Initialize the project

---
+++ src/hkust/comp3111h/focus/FatherFocusActivity.java
@@ -0,0 +1,13 @@
+package hkust.comp3111h.focus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FatherFocusActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
