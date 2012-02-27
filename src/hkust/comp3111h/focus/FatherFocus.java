e686486ce5cb5cf2a8c2f85a573ac40ce02c2d2d
Yiufung Cheong <yzhangak@ust.hk>
2012-02-27 23:50:00 +0800

    Test activity added

---
+++ src/hkust/comp3111h/focus/FatherFocus.java
@@ -0,0 +1,13 @@
+package hkust.comp3111h.focus;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class FatherFocus extends ListActivity {
+	//Testing.
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.main);
+	    }
+}
