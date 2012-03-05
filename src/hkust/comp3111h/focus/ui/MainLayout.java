273c3ca710dec164efb9951f6ad0ea58666c5e49
LI Tao <litao912002@gmail.com>
2012-03-05 23:06:33 +0800

    basic structure

---
+++ src/hkust/comp3111h/focus/ui/MainLayout.java
@@ -0,0 +1,8 @@
+package 
+import android.wiget.HorizontalScrollView;
+public MainLayout extends HorizontalScrollView {
+  private static final int SWIPE_MIN_DISTANCE = 5;
+  private static final int SWIPE_THRESHOLD_VELOCITY = 300;
+
+  private
+}
