3db2a0f24fbcd648d2847808cb6e72b5daecf844
LI Tao <litao912002@gmail.com>
2012-03-07 18:20:08 +0800

    Use actionbar style of tabs instead of tab host

--- src/hkust/comp3111h/focus/Activity/MainActivity.java
+++ src/hkust/comp3111h/focus/Activity/MainActivity.java
@@ -1,85 +1,82 @@
+/**
+ * @file MainActivity.java
+ * The main activity that will be shown when user start up
+ * Three basic framents navigated by tabs
+ * Using the library of ActionBarSherlock
+ */
 package hkust.comp3111h.focus.Activity;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
 
-import android.util.Log;
-import android.content.Context;
+import hkust.comp3111h.focus.R;
+import hkust.comp3111h.focus.ui.PagerAdapter;
+import hkust.comp3111h.focus.ui.StatisticsFragment;
+import hkust.comp3111h.focus.ui.TaskManageFragment;
+import hkust.comp3111h.focus.ui.TimerFragment;
+import android.animation.ObjectAnimator;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+
+import android.support.v4.app.ActionBar;
+import android.support.v4.app.ActionBar.OnNavigationListener;
+import android.support.v4.app.ActionBar.Tab;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.Menu;
+import android.support.v4.view.MenuItem;
 import android.support.v4.view.ViewPager;
+
 import android.view.View;
-import android.widget.TabHost;
-import android.widget.TabHost.TabContentFactory;
 
-import hkust.comp3111h.focus.R;
-import hkust.comp3111h.focus.ui.*;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.RotateAnimation;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends FragmentActivity implements 
-TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
-  private TabHost mTabHost;
+
+public class MainActivity extends FragmentActivity 
+  implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
+  //determine honecome or not
+  static final boolean IS_HONEYCOMB = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+
+  private final Handler handler = new Handler();
   private ViewPager mViewPager;
-  private HashMap<String, TabInfo> mapTabInfo = 
-      new HashMap<String, MainActivity.TabInfo>();
   private PagerAdapter mPagerAdapter;
 
-  /**
-   * Maintains extrinsic info of a tab's construct
+  //Actionbar set up
+  private boolean useLogo = false;
+  private boolean showHomeUp = false;
+
+  /** 
+   * Called when the activity is first created 
+   * Initialize Environment
    */
-  private class TabInfo {
-    private String tag;
-    private Class<?> clss;
-    private Bundle args;
-    private Fragment fragment;
-    TabInfo(String tag, Class<?> clazz, Bundle args) {
-      this.tag = tag;
-      this.clss = clazz;
-      this.args = args;
-    }
-  }
-  /**
-   * A simple factory that currently returns dummy views to the Tabhost
-   */
-  class TabFactory implements TabContentFactory {
-    private final Context mContext;
-
-    public TabFactory(Context context) {
-      mContext = context;
-    }
-
-    public View createTabContent(String tag) {
-      View v = new View(mContext);
-      v.setMinimumWidth(0);
-      v.setMinimumHeight(0);
-      return v;
-    }
-  }
-
-  protected void onCreate(Bundle savedInstanceState) {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    //Inflate the layout
-    setContentView(R.layout.pagerlayout);
-    //Initialise the TabHost
-    this.initialiseTabHost(savedInstanceState);
-    if(savedInstanceState != null) {
-      //set the tab as the saved state
-      mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
-    }
+    setContentView(R.layout.pagerlayout); //Basically a easy linear layout
+    final ActionBar ab = getSupportActionBar();
+    //Initialise ActionBar
+    //Set defaults for logo and home up
+    ab.setDisplayHomeAsUpEnabled(showHomeUp);
+    ab.setDisplayUseLogoEnabled(useLogo);
+    ab.setDisplayShowTitleEnabled(false);
+
+    //Set up tabs navigation
+    ab.addTab(ab.newTab().setText(R.string.tab1_name).setTabListener(this));
+    ab.addTab(ab.newTab().setText(R.string.tab2_name).setTabListener(this));
+    ab.addTab(ab.newTab().setText(R.string.tab3_name).setTabListener(this));
+    ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
     //Initialise ViewPager
     this.initialiseViewPager();
   }
 
-  protected void onSaveInstanceState(Bundle outState) {
-    //save the tab selected
-    outState.putString("tab", mTabHost.getCurrentTabTag());
-    super.onSaveInstanceState(outState);
-  }
-
   /**
-   * Initialise ViewPager
+   * Initialize ViewPager
    */
+  
   private void initialiseViewPager() {
     List<Fragment> fragments = new Vector<Fragment>();
     fragments.add(Fragment.instantiate(this, TaskManageFragment.class.getName()));
@@ -90,79 +87,52 @@ TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
     this.mViewPager.setAdapter(this.mPagerAdapter);
     this.mViewPager.setOnPageChangeListener(this);
   }
-
   /**
-   * Initialise the Tab Host
+   * Creating the action bar menu, redundent currently
    */
-  private void initialiseTabHost(Bundle args) {
-    mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-    mTabHost.setup();
-    TabInfo tabInfo = null;
-    MainActivity.AddTab(
-        this, 
-        this.mTabHost,
-        this.mTabHost.newTabSpec("Tasks").setIndicator("Tasks"), 
-        (tabInfo = new TabInfo("Tasks", TimerFragment.class, args)));
-    this.mapTabInfo.put(tabInfo.tag, tabInfo);
-
-    MainActivity.AddTab(
-        this, 
-        this.mTabHost,
-        this.mTabHost.newTabSpec("Timer").setIndicator("Timer"), 
-        (tabInfo = new TabInfo("Timer", TimerFragment.class, args)));
-    this.mapTabInfo.put(tabInfo.tag, tabInfo);
-
-    MainActivity.AddTab(
-        this, 
-        this.mTabHost,
-        this.mTabHost.newTabSpec("Statistics").setIndicator("Statistics"), 
-        (tabInfo = new TabInfo("Statistics", StatisticsFragment.class, args)));
-    this.mapTabInfo.put(tabInfo.tag, tabInfo);
-
-    //this.onTabChanged("Timer");
-    mTabHost.setOnTabChangedListener(this);
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    return super.onCreateOptionsMenu(menu);
   }
 
-  /**
-   * Add Tab content to the Tabhost
-   * @param activity
-   * @param tabHost
-   * @param tabSpec
-   * @Param clss
-   * @Param args
-   */
-  private static void AddTab(
-      MainActivity activity, 
-      TabHost tabHost, 
-      TabHost.TabSpec tabSpec,
-      TabInfo tabInfo) {
-    tabSpec.setContent(activity.new TabFactory(activity));
-    tabHost.addTab(tabSpec);
+  @Override 
+  public boolean onOptionsItemSelected(MenuItem item) {
+    return super.onOptionsItemSelected(item);
   }
 
-  public void onTabChanged(String tag) {
-    //TabInfo newTab = this.mapTabInfo.get(tag);
-    int pos = this.mTabHost.getCurrentTab();
-    this.mViewPager.setCurrentItem(pos);
+
+  public void onTabReselected(Tab tab, FragmentTransaction ft) {
+// TODO Auto-generated method stub
+  }	
+	
+  public void onTabSelected(Tab tab, FragmentTransaction ft) {
+// TODO Auto-generated method stub
+	if(mViewPager==null){
+	  this.initialiseViewPager();
+	}
+	mViewPager.setCurrentItem(tab.getPosition());
+  }
+	
+  public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+  // TODO Auto-generated method stub
   }
 
-  public void onPageScrolled(int position, float positionOffset,
+public void onPageScrolled(int position, float positionOffset,
 		int positionOffsetPixels) {
 	// TODO Auto-generated method stub
 	
-  }
-
-  public void onPageSelected(int position) {
-	// TODO Auto-generated method stub
-	  this.mTabHost.setCurrentTab(position);
-	
-  }
-
-  public void onPageScrollStateChanged(int state) {
-	// TODO Auto-generated method stub
-	
-  }
-
-
 }
 
+public void onPageSelected(int position) {
+	// TODO Auto-generated method stub
+    final ActionBar ab = getSupportActionBar();
+    ab.setSelectedNavigationItem(position);
+	
+}
+
+public void onPageScrollStateChanged(int state) {
+	// TODO Auto-generated method stub
+	
+}
+  
+}
