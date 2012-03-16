51279940f63a81d79277a11cf0112bea1a3b530b
LI Tao <litao912002@gmail.com>
2012-03-16 21:53:00 +0800

    back to swipe style UI, changed the color

---
+++ src/hkust/comp3111h/focus/ui/MainViewPager.java
@@ -0,0 +1,20 @@
+package hkust.comp3111h.focus.ui;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+import hkust.comp3111h.focus.ui.*;
+import hkust.comp3111h.focus.Adapter.*;
+
+
+public class MainViewPager extends ViewPager {
+  public MainViewPager(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+  @Override
+  public void setAdapter(PagerAdapter adapter) {
+    super.setAdapter(adapter);
+  }
+}
