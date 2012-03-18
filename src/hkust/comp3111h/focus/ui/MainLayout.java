374bf8b43521af1a810d1a351fa37a260766043d
LI Tao <litao912002@gmail.com>
2012-03-18 13:48:50 +0800

    delete unneeded files

--- src/hkust/comp3111h/focus/ui/MainLayout.java
+++
@@ -1,8 +0,0 @@
-package 
-import android.wiget.HorizontalScrollView;
-public MainLayout extends HorizontalScrollView {
-  private static final int SWIPE_MIN_DISTANCE = 5;
-  private static final int SWIPE_THRESHOLD_VELOCITY = 300;
-
-  private
-}
