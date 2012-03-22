9f4e97d39dd6a432cf518a7fd90a1383d037149b
Yiufung Cheong <yzhangak@ust.hk>
2012-03-22 11:16:40 +0800

    JUnit test file added, waiting for implementation

---
+++ src/hkust/comp3111h/focus/test/TaskDbAdapterTest.java
@@ -0,0 +1,116 @@
+package hkust.comp3111h.focus.test;
+
+import static org.junit.Assert.fail;
+import hkust.comp3111h.focus.database.TaskDbAdapter;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TaskDbAdapterTest {
+	
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testCreateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchAllUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserSchema() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateTaskList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchTaskList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchAllTaskLists() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteTaskList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateTaskList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTaskListSchema() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateTask() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchTask() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchAllTasks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchAllTasksInList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteTask() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateTask() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTaskSchema() {
+		fail("Not yet implemented");
+	}
+
+}
