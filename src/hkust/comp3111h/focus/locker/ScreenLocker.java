48793dbf834472ebd3673fe26f414ae3741a1d16
Gary Cheung <gary1412zhang@gmail.com>
2012-04-17 01:30:22 +0800

    Popup constructed

--- src/hkust/comp3111h/focus/locker/ScreenLocker.java
+++ src/hkust/comp3111h/focus/locker/ScreenLocker.java
@@ -7,13 +7,17 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class ScreenLocker {
   private DevicePolicyManager dpm;
@@ -21,6 +25,10 @@ public class ScreenLocker {
   private Activity activity;
   private CountDownTimer cdt;
   private PopupWindow pw;
+  View popview;
+  TextView pop_relock;
+  EditText et;
+  TextView tv;
 	
   public ScreenLocker(Activity activity) {
     this.activity = activity;
@@ -30,33 +38,99 @@ public class ScreenLocker {
   
   private void popup() {
 	LayoutInflater mLayoutInflater = (LayoutInflater) activity.getSystemService(activity.LAYOUT_INFLATER_SERVICE);
-	View popview = mLayoutInflater.inflate(R.layout.pop_background, null);
+	popview = mLayoutInflater.inflate(R.layout.pop_background, null);
 	pw = new PopupWindow(popview, LayoutParams.MATCH_PARENT,
                          LayoutParams.MATCH_PARENT, true);
 	pw.setAnimationStyle(R.style.PopWindowAnimation);
 	View p = mLayoutInflater.inflate(R.layout.main, null);
 	pw.showAtLocation(p, Gravity.CENTER | Gravity.CENTER, 0, 0);
+
+	pop_relock = (TextView)popview.findViewById(R.id.relock_time);
+
+	et = (EditText)popview.findViewById(R.id.pop_input);
+	tv = (TextView)popview.findViewById(R.id.unlock_sequence);
+	et.addTextChangedListener(new TextWatcher() {
+
+		@Override
+		public void afterTextChanged(Editable arg0) {
+		  cdt.cancel();
+		  pop_relock.setText("Or it will be locked soon!");
+		  new CountDownTimer(5000, 1000) {
+
+			@Override
+			public void onFinish() {
+			  cdt.start();
+			}
+
+			@Override
+			public void onTick(long millisUntilFinished) {
+				// TODO Auto-generated method stub
+				
+			}
+			  
+		  }.start();
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	});
 	
-	Button good_button = (Button)popview.findViewById(R.id.good_button);
+	Button lazy_button = (Button)popview.findViewById(R.id.lazy_button);
+	Button gary_lee_button = (Button)popview.findViewById(R.id.gary_lee_button);
 	OnClickListener listener = new OnClickListener() {
 	  @Override
 	  public void onClick(View v) {
-   	    pw.dismiss();
+   	    switch(v.getId()) {
+   	      case R.id.lazy_button:
+   	    	try_escape();
+   	    	break;
+   	      case R.id.gary_lee_button:
+   	    	sysLock();
+   	    	cdt.cancel();
+   	    	pop_relock.setText("Or it will be locked soon!");
+   	    	break;
+   	    }
 	  }
 	};
-	good_button.setOnClickListener(listener);
-	
-	
+	lazy_button.setOnClickListener(listener);
+	gary_lee_button.setOnClickListener(listener);
+  }
+  
+  private void try_escape() {
+	String et_string = et.getText().toString();
+	String tv_string = tv.getText().toString(); 
+	if (et_string.equals(tv_string)) {
+	  pw.dismiss();
+	  cdt.cancel();
+	} else {
+      et.setText("");
+	}
   }
   
   public void lock() {
 	popup();
 	cdt = new CountDownTimer(10000, 1000) {
 	  public void onTick(long millisUntilFinished) {
-		
+		pop_relock.setText("Or it will be locked in "+ Long.toString(millisUntilFinished / 1000) +
+				           " seconds!");
+		pw.update();
       }
 
 	  public void onFinish() {
+		pop_relock.setText("Or it will be locked soon!"); 
+		pw.update();
 	    sysLock();
 	  }
 	};
